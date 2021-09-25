734bebb237420126ccf76710b76b07daadfa8b1a
GyuuTomioka <silasvljunior@gmail.com>
2021-09-25 15:03:51 -0300

    tope

---
+++ src/imagens.js
@@ -0,0 +1,28 @@
+const imagens = (prefix) => {
+	return `
+-ツ♦IMAGENS♦ツ-
+♢➤ *${prefix}cringe*
+♢➤ *${prefix}pinterest (sua pesquisa)*
+♢➤ *${prefix}shit*
+♢➤ *${prefix}ler [comente a imagem]*
+♢➤ *${prefix}bomdia* 
+♢➤ *${prefix}boatarde*
+♢➤ *${prefix}boanoite* 
+-ツ♦COMENTE A IMAGEM♦ツ-
+♢➤ *${prefix}hitler* 
+♢➤ *${prefix}trash* 
+♢➤ *${prefix}trash2* 
+♢➤ *${prefix}bomdia* 
+♢➤ *${prefix}morto* 
+♢➤ *${prefix}joke* 
+♢➤ *${prefix}animerosto* 
+♢➤ *${prefix}quadro* 
+♢➤ *${prefix}afetar* 
+-ツ♦ANIME♦ツ-
+♢➤ *${prefix}loli*
+♢➤ *${prefix}neko* [+18]
+♢➤ *${prefix}hentai* [+18]
+`
+}
+
+exports.imagens = imagens
